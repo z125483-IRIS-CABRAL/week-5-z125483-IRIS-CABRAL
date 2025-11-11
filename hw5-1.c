c5c1648f8613b7ba155001a435777f1c95667311
Z125483-IRIS-CABRAL <z125483@shibaura-it.ac.jp>
2025-11-11 23:11:34 +0900

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main()
+{
+    int maxNum;
+    printf("max? ");
+    scanf("%d", &maxNum);
+    
+    int power = 1;
+    while(power < maxNum){
+        printf("%d ", power);
+        power *= 2;
+    }
+    
+    printf("\n");
+
+    return 0;
+}
