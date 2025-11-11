9acec26c4241feeb3aadc72bea16f063c62c4ef3
Z125483-IRIS-CABRAL <z125483@shibaura-it.ac.jp>
2025-11-11 23:45:33 +0900

    Update ahw5-1.c

--- ahw5-1.c
+++ ahw5-1.c
@@ -1,1 +1,33 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int n, m;
+    scanf("%d %d", &n, &m);
+    
+    if(n > m){
+        printf("Cannot transform %d to %d\n", n, m);
+        return 0;
+    }
+    
+    int ans = 0;
+
+    while(m > n){
+        if(m%2 ==0){
+            m = m/2;
+        } else{
+            m = m - 1;
+        }
+        ans+=1;
+    }
+
+    if(m == n){
+        printf("Numbers of steps taken: %d", ans);
+    } else{
+        printf("Cannot transform %d to %d", n, m);
+    }
+ 
+    printf("\n");
+    return 0;
+}
