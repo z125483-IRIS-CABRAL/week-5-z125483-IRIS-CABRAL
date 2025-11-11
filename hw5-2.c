2f409d0390ffcdc5668497c175bb261b45683fda
Z125483-IRIS-CABRAL <z125483@shibaura-it.ac.jp>
2025-11-11 23:22:11 +0900

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("max? ");
+    scanf("%d", &num);
+    
+    int divisor = 1;
+    while(divisor <= num){
+        if(num % divisor == 0){
+            printf("%d ", divisor);
+            divisor+=1;
+        } else{
+            divisor += 1;
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
