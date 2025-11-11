451fa63b736634754f76470108e5f83810ced3f9
Z125483-IRIS-CABRAL <z125483@shibaura-it.ac.jp>
2025-11-11 23:31:25 +0900

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    printf("Please enter two different numbers \n");
+    scanf("%d %d", &num1, &num2);
+
+    while(num1 == num2){
+        printf("Same, please enter two different numbers \n");
+        printf("Input: ");
+        scanf("%d %d", &num1, &num2);
+    }
+    
+    printf("You entered %d and %d", num1, num2);
+    
+
+    printf("\n");
+    return 0;
+}
